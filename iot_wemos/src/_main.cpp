e625862f387a1207daa59d18df3873623d7ec407
CMOW5 <tec.cmos@gmail.com>
2018-07-05 21:46:13 -0500

    test main

---
+++ iot_wemos/src/_main.cpp
@@ -0,0 +1,20 @@
+
+/*
+#include "../lib/App/src/App.h"
+
+App app;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("setup");
+  // Initialize all app modules, including application(s).
+  app.initialize();
+  // APP_Initialize();
+}
+
+void loop() {
+  // state machine
+  // APP_Tasks();
+  app.tasks();
+}
+*/
