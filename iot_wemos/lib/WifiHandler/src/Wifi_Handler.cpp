6de5c34c380a1602895fd92f8ec20c85b054e40d
CMOW5 <tec.cmos@gmail.com>
2018-07-06 03:27:46 -0500

    config singleton added

--- iot_wemos/lib/WifiHandler/src/Wifi_Handler.cpp
+++ iot_wemos/lib/WifiHandler/src/Wifi_Handler.cpp
@@ -1,39 +1,18 @@
 
 #include "Wifi_Handler.h"
 
-
 WifiHandler::WifiHandler() {
-  // ...
-
-  mqtt = new Adafruit_MQTT_Client(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_USERNAME, MQTT_KEY);
-  temperatureFeed = new Adafruit_MQTT_Publish(mqtt, MQTT_FEED_TEMP);
-  humidityFeed = new Adafruit_MQTT_Publish(mqtt, MQTT_FEED_HUMI);
+	systemConfig = SystemConfig::instance();
 }
 
 void WifiHandler::connectWifi()
 {
-	const char* SSID = "192.168.0.13";
-	const char* SSPASSWORD = "91121608540";
-	WiFi.begin(SSID, SSPASSWORD);
+	WiFi.begin(systemConfig->WIFI_SSID, systemConfig->WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
      delay(500);
      Serial.print(".");
-		 Serial.print(WiFi.status());
   }
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-}
-
-void WifiHandler::connectMQTT()
-{
-  if (mqtt->connected())
-   return;
- 	Serial.print("Connecting to MQTT... ");
-	while (mqtt->connect() != 0) {
-		Serial.println("Error. Retrying MQTT connection in 5 seconds...");
-	  mqtt->disconnect();
-	  delay(5000);
-	 }
-	 Serial.print("MQTT connected ");
 }
--- iot_wemos/lib/WifiHandler/src/Wifi_Handler.h
+++ iot_wemos/lib/WifiHandler/src/Wifi_Handler.h
@@ -2,36 +2,21 @@
 #define _WIFI_HANDLER_H_
 
 #include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+#include "system_config.h"
 
-// #define SSID      "192.168.0.13"
-// #define SSPASSWORD  "91121608540"
-
-#define MQTT_SERVER      "192.168.0.13"
-#define MQTT_SERVERPORT  1883
-#define MQTT_USERNAME    ""
-#define MQTT_KEY         ""
-#define MQTT_FEED_TEMP   "ies/aula20/temperature"
-#define MQTT_FEED_HUMI   "ies/aula20/humidity"
+#define SSID "TEC4825"
+#define SSPASSWORD "91121608540"
 
 class WifiHandler
 {
-  WiFiClient client;
-  Adafruit_MQTT_Client* mqtt;
-  Adafruit_MQTT_Publish* temperatureFeed;
-  Adafruit_MQTT_Publish* humidityFeed;
-
-  /*
-  Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_USERNAME, MQTT_KEY);
-  Adafruit_MQTT_Publish temperatureFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_FEED_TEMP);
-  Adafruit_MQTT_Publish humidityFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_FEED_HUMI);
-  */
-
   public:
+    WiFiClient client;
+    // methods
     WifiHandler();  // This is the constructor
     void connectWifi(void);
-    void connectMQTT(void);
+
+  private:
+    SystemConfig* systemConfig;
 };
 
 #endif
