b877504837112267df246677b7db8e93a641e00c
CMOW5 <tec.cmos@gmail.com>
2018-07-06 17:53:11 -0500

    events names changed

--- iot_wemos/lib/App/src/App.cpp
+++ iot_wemos/lib/App/src/App.cpp
@@ -10,19 +10,20 @@ void App::initialize()
   appState = 1;
   // initialize something
   // SYS_Initialize(); //initialize the system
+  Serial.begin(9600);
 
   // pin init
   pins.D5.mode(INPUT_PULLUP);
   pins.D6.mode(INPUT_PULLUP);
+  pins.D4.mode(OUTPUT);
+  digitalWrite(pins.D4.getValue(), LOW);
 
   // Events
-  attachInterrupt(digitalPinToInterrupt(pins.D5.getValue()), Events::event1, FALLING);
-  attachInterrupt(digitalPinToInterrupt(pins.D6.getValue()), Events::event2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(pins.D5.getValue()), Events::eventButton1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(pins.D6.getValue()), Events::eventButton2, FALLING);
 
-  Serial.begin(9600);
   delay(3000);
   Serial.print("App initialized");
-  Serial.print(pins.D6.getValue());
   wifiHandler.connectWifi();
   mqttHandler.connectMQTT();
 }
@@ -60,13 +61,13 @@ void App::processEvents()
 {
   if(events.event_btn1) {
     events.event_btn1 = false;
-    Serial.print("event btn1");
+    Serial.println("event btn1");
     mqttHandler.temperatureFeed->publish("temp");
   }
 
   if(events.event_btn2) {
     events.event_btn2 = false;
-    Serial.print("event btn2");
+    Serial.println("event btn2");
     mqttHandler.humidityFeed->publish("hum");
   }
 }
