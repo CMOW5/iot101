5e9ac6e50f1d6a35063216046bc10419fba38c6a
CMOW5 <tec.cmos@gmail.com>
2018-07-09 05:04:18 -0500

    mqttHandler is now a class instead of a single module

--- iot_server/mqtt-con/mqttApi.js
+++ iot_server/mqtt-con/mqttApi.js
@@ -1,5 +1,6 @@
 var mqtt = require('mqtt');
 
+/*
 var mqttApi = {
   MQTT_SERVER: '192.168.0.13',
   // MQTT_SERVER = 'test.mosquitto.org',
@@ -16,4 +17,22 @@ client.on('connect', function () {
 
 mqttApi.client = client;
 
+//client.publish('presence', 'Hello mqtt')
+
+
 module.exports = mqttApi;
+*/
+
+export default class MqttHandler {
+  constructor() {
+    this.MQTT_SERVER = '192.168.0.13',
+    this.MQTT_TOPIC_TEMPERATURE = 'ies/aula20/temperature',
+    this.MQTT_TOPIC_HUMIDITY = 'ies/aula20/humidity'
+    this.client = mqtt.connect('mqtt://' + this.MQTT_SERVER);
+    this.client.on('connect', () => {
+        this.client.subscribe(this.MQTT_TOPIC_TEMPERATURE);
+        this.client.subscribe(this.MQTT_TOPIC_HUMIDITY);
+    });  
+    
+  }
+}
