ee8ba81a540887ba883a3d3b9b5dfdafe90157cc
CMOW5 <tec.cmos@gmail.com>
2018-07-09 05:03:19 -0500

    two separates config, one for the server and one for the front end app

--- iot_server/webpack.config.js
+++ iot_server/webpack.config.js
@@ -1,44 +1,4 @@
 /*
-const webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
-
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: 'build.js'
-  },
-  target: 'node',
-  node: {
-    __dirname: false
-  },
-  externals: nodeExternals(),
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: `'production'`
-      }
-    }),
-    new HtmlWebpackPlugin({
-      template: "./index.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        },
-      }
-    ]
-  }
-};
-*/
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -67,3 +27,59 @@ module.exports = {
     })
   ]
 };
+*/
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var nodeExternals = require('webpack-node-externals');
+
+var config = {
+  // common configuration
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+}
+
+var serverConfig = Object.assign({}, config, {
+  name: "server",
+  entry: "./bin/www",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "server-bundle.js"
+  },
+  target: 'node',
+  node: {
+      // fs: 'empty',
+    __dirname: true
+  },
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder 
+});
+
+var frontEndConfig = Object.assign({}, config, {
+  name: "frontEnd",
+  entry: "./views/main/index.js",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "index-bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./views/main/index.html"
+    })
+  ]
+});
+
+// Return Array of Configurations
+module.exports = [
+  serverConfig, frontEndConfig,    	
+];
