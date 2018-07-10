6f00e3be715c3a5c8add6a8e9ed0aba3f87c8bd9
CMOW5 <tec.cmos@gmail.com>
2018-07-10 00:10:31 -0500

    is now possible to add new channels and callbacks dynamically

--- iot_server/socket/socketApi.js
+++ iot_server/socket/socketApi.js
@@ -1,18 +1,33 @@
 var socket_io = require('socket.io');
 var io = socket_io();
-var socketApi = {};
 
+var socketApi = {};
 socketApi.io = io;
+socketApi.listeners = {};
 
 io.on('connection', function(socket){
     console.log('A device has connected');
-    socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
-    });
+    for (let channel in socketApi.listeners) {
+      const callback = socketApi.listeners[channel];
+      socket.on(channel, callback);
+    }
 });
 
-socketApi.sendNotification = function() {
-  io.sockets.emit('hello', {msg: 'Hello World!'});
+socketApi.addListener = function(channel, callback) {
+  socketApi.listeners[channel] = callback;
+}
+
+socketApi.removeListener = function(channel) {
+  delete socketApi.listeners[channel];
+}
+
+socketApi.clearListeners = function() {
+  socketApi.listeners = {};
+}
+
+socketApi.sendNotification = function(channel, message) {
+  io.emit(channel, message);
+  // io.sockets.emit('hello', {msg: 'Hello World!'});
 }
 
 module.exports = socketApi;
