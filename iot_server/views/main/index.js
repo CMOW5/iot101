0668304ce34206a30e7487fbdaf8ac9d23f06c80
CMOW5 <tec.cmos@gmail.com>
2018-07-10 22:21:03 -0500

    bulma dependency added

--- iot_server/views/main/index.js
+++ iot_server/views/main/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 
+/* styles */
+import 'bulma/css/bulma.css';
+
 ReactDOM.render(<App />, document.getElementById("root"));
