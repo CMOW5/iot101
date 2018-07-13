bf66f27e1c48f1cce1b073e99c2b9fcacd0e7a2b
CMOW5 <tec.cmos@gmail.com>
2018-07-13 01:10:39 -0500

    pins initialization added

--- iot_wemos/src/main.cpp
+++ iot_wemos/src/main.cpp
@@ -1,6 +1,6 @@
-
-
+#include "Arduino.h"
 #include "App.h"
+#include "Pins.h"
 #include "Wifi_Handler.h"
 #include "MQTT_Handler.h"
 #include "IOHandler.h"
@@ -8,24 +8,37 @@
 WifiHandler wifiHandler;
 MQTTHandler mqttHandler(wifiHandler);
 IOHandler ioHandler;
+Pins *pins = new Pins();
 
 App* app = new App(
+  pins,
   &mqttHandler,
   &wifiHandler,
   mqttHandler.getEvents(),
   ioHandler.getEvents()
 );
 
+void initPins(void);
+
 void setup() {
+  initPins();
   app->initialize();
-  wifiHandler.connectWifi();
+  wifiHandler.connect();
   mqttHandler.initialize();
-  mqttHandler.connectMQTT();
+  mqttHandler.connect();
 }
 
 void loop() {
   // the app state machine
   // app.tasks()
-  mqttHandler.connectMQTT(); // keep the mqtt connection alive
+  mqttHandler.connect(); // keep the mqtt connection alive
   mqttHandler.processSubscriptions(); // keep watching for mqqt subcriptions events
+}
+
+void initPins() {
+  pins->D5.mode(INPUT_PULLUP);
+  pins->D6.mode(INPUT_PULLUP);
+  pins->D1.mode(OUTPUT);
+  pins->D2.mode(OUTPUT);
+  pins->D3.mode(OUTPUT);
 }
