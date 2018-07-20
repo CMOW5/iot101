8c6f8e5c3de8ed57cf1dd30d35c7ae3382510299
CMOW5 <tec.cmos@gmail.com>
2018-07-20 02:08:55 -0500

    intermitent method added to turn on and off and output pin

--- iot_wemos/lib/Pins/src/Pin.cpp
+++ iot_wemos/lib/Pins/src/Pin.cpp
@@ -1,7 +1,22 @@
 #include "Pin.h"
 
+bool Pin::toogleStatus = false;
+
 Pin::Pin(byte value) {
   pinVal = value;
+}
+
+Pin::~Pin() {
+  flipper.detach();
+}
+
+void toggle(byte pin) {
+  int reading = digitalRead(pin);
+  if (reading == 0) {
+    digitalWrite(pin, HIGH);
+  } else {
+    digitalWrite(pin, LOW);
+  }
 }
 
 // Initialize the aplication modules
@@ -11,11 +26,17 @@ void Pin::mode(uint8_t mode)
 }
 
 void Pin::turnOn() {
+  flipper.detach(); // if the led was intermitent, stop the flipper
   digitalWrite(pinVal, HIGH);
 }
 
 void Pin::turnOff() {
+  flipper.detach(); // if the led was intermitent, stop the flipper
   digitalWrite(pinVal, LOW);
+}
+
+void Pin::intermitent() {
+  flipper.attach(0.3, toggle, pinVal);
 }
 
 // Initialize the aplication modules
--- iot_wemos/lib/Pins/src/Pin.h
+++ iot_wemos/lib/Pins/src/Pin.h
@@ -2,18 +2,24 @@
 #define _PIN_H_
 
 #include <Arduino.h>
+#include <Ticker.h>
 
 class Pin
 {
   byte pinVal;
+  Ticker flipper;
+  static bool toogleStatus;
 
   public:
     // methods
     Pin(byte value);
+    ~Pin();
     byte value();
     void mode(uint8_t mode);
     void turnOn(void);
     void turnOff(void);
+    void intermitent(void);
+    // static void toggle(Pin*);
 };
 
 #endif
