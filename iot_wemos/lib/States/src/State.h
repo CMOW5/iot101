8880b1d8dd23a839b430efb253aefcaa4fc333fb
CMOW5 <tec.cmos@gmail.com>
2018-07-20 02:07:20 -0500

    actions added

--- iot_wemos/lib/States/src/State.h
+++ iot_wemos/lib/States/src/State.h
@@ -5,8 +5,11 @@
 class State {
    public:
       // pure virtual function providing interface framework.
+      // virtual void datosCargados() = 0;
       virtual void solicitarServicio() = 0;
-      virtual void confirmado() = 0;
+      virtual void pedidoTomado() = 0;
+      virtual void atendido() = 0;
+      virtual void prenderAlarma() = 0;
 };
 
 #endif
