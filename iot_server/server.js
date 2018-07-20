a13fdb75d452bdddcfcd12120ae94abcde252a91
CMOW5 <tec.cmos@gmail.com>
2018-07-20 02:03:10 -0500

    cors added

--- iot_server/server.js
+++ iot_server/server.js
@@ -2,11 +2,20 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 var logger = require('morgan');
+
+/* mqtt config */
 const mqttConfig = require('./mqtt-con/mqtt-config');
+
+/* database config */
+var dbconnection = require('./database/connection');
+var Mesa = require('./models/mesa');
+
 
 /* routes */
 var indexRouter = require('./routes/index');
+var mesaRouter = require('./routes/mesa');
 var testRouter = require('./routes/test');
 
 var app = express();
@@ -15,6 +24,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 /* routes */
 app.use('/', indexRouter);
+app.use('/mesa', mesaRouter);
 app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
@@ -31,6 +42,26 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+Mesa.findById(1).then(mesa => {
+  console.log('mesa = ', mesa.estado);
+});
+
+// Mesa.testfunct();
+
+/* db config */
+/* dbconnection
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+        // search for known ids
+    Mesa.findById(1).then(mesa => {
+      console.log('mesa = ', mesa.estado);
+    });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+ */
 /* mqtt config */
 mqttConfig(app);
 
