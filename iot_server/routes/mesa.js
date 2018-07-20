6584220fdaa04586afb222744ef27aad47bd6ae0
CMOW5 <tec.cmos@gmail.com>
2018-07-20 02:00:16 -0500

    the mesa api

---
+++ iot_server/routes/mesa.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* repositories */
+var Mesa = require('../models/mesa');
+
+/* GET home page. */
+router.get('/', function(req, res, next) { 
+  // res.json({ user: 'tobi' });
+  const mesaId = 1; // find the mesa id here
+  Mesa.findById(mesaId).then((mesa) => {
+    res.json(mesa);
+  });
+});
+
+module.exports = router;
