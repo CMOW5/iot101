e3d2a318b1d1ef195e90a7d7ac1bb6967eb4a011
CMOW5 <tec.cmos@gmail.com>
2018-07-20 20:52:10 -0500

    mesas loaded from db via api

--- iot_server/views/main/App.js
+++ iot_server/views/main/App.js
@@ -2,24 +2,22 @@ import React, {Component} from 'react';
 
 /* services */
 const socketHandler = require('./services/socket/socket-handler');
-const axios = require('axios');
-
-// import openSocket from 'socket.io-client';
-// const socket = openSocket('http://localhost:3000');
+import mesasRequest from './services/api/mesas/mesas-request';
 
 /* components */
 import MainTable from './components/main_table/MainTable';
+import Loading from './components/utils/loading/Loading';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       mesas: [
-        {number: 1, state: 'disponible'}, 
+        {id: 1, state: 'disponible'}, 
         // {number: 2, state: 'solicitando_servicio'}
       ],
+      isFetching: true,
     }
-    this.handleClick = this.handleClick.bind(this);
     this.updateMesas = this.updateMesas.bind(this);
     this.selectedAction = this.selectedAction.bind(this);
     this.loadMesas = this.loadMesas.bind(this);
@@ -36,27 +34,19 @@ class App extends Component {
     this.loadMesas();
   }
 
-  loadMesas() {
-    console.log('get mesas');
-    // Make a request for a user with a given ID
-    axios.get('http://localhost:3000/mesa')
-    .then((response) => {
-      const mesa = response.data;
-      this.setState({
-        mesas: [
-          {number: mesa.id, state: mesa.estado},
-        ],
-      });
-    })
-    .catch(function (error) {
-      console.log('error = ', error);
-      console.log(error);
+  async loadMesas() {
+    const mesa = await mesasRequest.fetchMesas();
+    this.setState({
+      mesas: [
+        {id: mesa.id, state: mesa.state},
+      ],
+      isFetching: false
     });
   }
 
   updateMesas(newState) {
     const newMesas = this.state.mesas.map((mesa) => {
-      if (mesa.number === 1) {
+      if (mesa.id === 1) {
         mesa.state = newState;
       }
       return mesa;
@@ -66,27 +56,24 @@ class App extends Component {
     });
   }
 
-  handleClick() {
-    socketHandler.sendNotification('chat message', 'message from socket');
-    // this.socket.emit('chat message', 'message from socket');
-    console.log('clicked');
-  }
-
   selectedAction(mesa, action) {
+    console.log('selected action = ', action);
     socketHandler.sendNotification('state_change', action);
     this.updateMesas(action);
   }
 
   render() {
     return (
-      <div>
-        <button onClick={this.handleClick}>send</button>
-        <MainTable onSelectedAction={this.selectedAction} mesas={this.state.mesas} />
+      <div className="container">
+        {
+          this.state.isFetching ? 
+            <Loading show={true} title="" />
+            : 
+            <MainTable onSelectedAction={this.selectedAction} mesas={this.state.mesas} />
+        }
       </div>
     );
   }
 }
-
-// const App = () => <div>💅</div>;
 
 export default App;
