5ba0f91647cb62b35fa727a79ad92969b715c58b
CMOW5 <tec.cmos@gmail.com>
2018-07-31 21:35:44 -0500

    eslint errors fixed

--- iot_server/models/user.js
+++ iot_server/models/user.js
@@ -1,9 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
+  let User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
   }, {});
   User.associate = function(models) {
     // associations can be defined here
