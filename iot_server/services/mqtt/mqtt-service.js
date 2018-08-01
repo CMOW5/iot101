43785d5d5ea505c3e7281d48a78cc08a0409bd94
CMOW5 <tec.cmos@gmail.com>
2018-07-31 23:57:09 -0500

    TODOS added

--- iot_server/services/mqtt/mqtt-service.js
+++ iot_server/services/mqtt/mqtt-service.js
@@ -2,15 +2,13 @@ let mqtt = require('mqtt');
 
 let mqttService = {
   MQTT_SERVER: '192.168.0.13',
-  MQTT_FEED_CAMBIAR_ESTADO: 'restaurante/mesa_1/estado',
-  MQTT_FEED_CARGAR_DATOS: 'restaurante/mesa_1/datos',
-  MQTT_ENVIAR_ESTADO: 'restaurante/mesa_1/server/estado',
 };
 
 let client = mqtt.connect('mqtt://' + mqttService.MQTT_SERVER);
 
 mqttService.client = client;
 
+// TODO: jsdoc this
 mqttService.publish = function(channel, message) {
   console.log('publishin to channel ' + channel + ' message = ' + message);
   client.publish(channel, message);
@@ -44,12 +42,11 @@ mqttService.getMesaIdFromTopic = function(topic) {
     let mesaId = parseInt(mesaName.substring(mesaName.indexOf('_') + 1));
     return mesaId;
   }
-
-  // if another topic
 };
 
 
 /* helpers */
+
 /**
  * get the mesa state topic
  *
