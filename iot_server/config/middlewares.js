5ecab819cce594d269ef810a9d323f04e5fc1fe7
CMOW5 <tec.cmos@gmail.com>
2018-07-31 21:32:03 -0500

    eslint errors fixed

--- iot_server/config/middlewares.js
+++ iot_server/config/middlewares.js
@@ -1,19 +1,19 @@
-// register the app middlewares here 
+// register the app middlewares here
 
 'use strict';
 
-var cookieParser = require('cookie-parser');
-var cors = require('cors');
-var logger = require('morgan');
-var express = require('express');
-var path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
 module.exports = (app) => {
   app.use(cors());
   app.use(logger('dev'));
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({extended: false}));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, '../public')));
   app.use(express.static(path.join(__dirname, '../build')));
-}
+};
