ed0f2af5a4583a8dc4ffcb502cd747c5eb98bb30
CMOW5 <tec.cmos@gmail.com>
2018-07-31 21:37:29 -0500

    eslint errors fixed

--- iot_server/services/socket/socket-service.js
+++ iot_server/services/socket/socket-service.js
@@ -1,32 +1,43 @@
-var socket_io = require('socket.io');
-var io = socket_io();
+const socketIO = require('socket.io');
+const io = socketIO();
 
-var socketService = {};
+const socketService = {};
 socketService.io = io;
 socketService.listeners = {};
 
-io.on('connection', function(socket){
-    console.log('A device has connected');
-    for (let channel in socketService.listeners) {
-      const callback = socketService.listeners[channel];
-      socket.on(channel, callback);
-    }
+const channels = {
+  STATE_CHANGE: 'state_change',
+};
+
+io.on('connection', function(socket) {
+  console.log('A device has connected');
+  Object.keys(socketService.listeners).forEach((channel) => {
+    const callback = socketService.listeners[channel];
+    socket.on(channel, callback);
+  });
 });
 
 socketService.addListener = function(channel, callback) {
   socketService.listeners[channel] = callback;
-}
+};
 
 socketService.removeListener = function(channel) {
   delete socketService.listeners[channel];
-}
+};
 
 socketService.clearListeners = function() {
   socketService.listeners = {};
-}
+};
 
 socketService.sendNotification = function(channel, message) {
   io.emit(channel, message);
-}
+};
+
+socketService.sendStateChange = function(mesaId, newState) {
+  io.emit(channels.STATE_CHANGE, {
+    mesaId: mesaId,
+    newState: newState,
+  });
+};
 
 module.exports = socketService;
