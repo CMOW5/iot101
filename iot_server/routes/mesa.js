755f6a7af7c068070a844efdea1096b10bab2bc9
CMOW5 <tec.cmos@gmail.com>
2018-08-18 18:33:04 -0500

    the index now returns all the mesas from db

--- iot_server/routes/mesa.js
+++ iot_server/routes/mesa.js
@@ -7,10 +7,16 @@ const Mesa = require('../models/mesa');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.json({ user: 'tobi' });
+  Mesa.findAll().then((mesas) => {
+    res.json({data: mesas});
+  });
+
+  /*
   const mesaId = 1; // find the mesa id here
   Mesa.findById(mesaId).then((mesa) => {
     res.json({data: mesa});
   });
+  */
 });
 
 module.exports = router;
