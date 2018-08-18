11801e650cc3fdfddf2629f3633c568e51a3f7cc
CMOW5 <tec.cmos@gmail.com>
2018-08-18 18:32:02 -0500

    config/config.json changed to dabatase.js

---
+++ iot_server/config/database.js
@@ -0,0 +1,28 @@
+// const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: '',
+    database: 'allix_iot_restaurante',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306,
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+    port: 3306,
+  },
+};
