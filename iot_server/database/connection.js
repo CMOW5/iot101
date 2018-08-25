091720c8ef773e96b765db53130b4794e682a3b7
CMOW5 <tec.cmos@gmail.com>
2018-08-24 23:22:12 -0500

    the db name, user and password are now loaded from database config

--- iot_server/database/connection.js
+++ iot_server/database/connection.js
@@ -1,17 +1,21 @@
 const Sequelize = require('sequelize');
 const dbconfig = require('../config/database.js');
 
-const dbconnection = new Sequelize('allix_iot_restaurante', 'root', '', {
-  host: dbconfig.development.host,
-  dialect: dbconfig.development.dialect,
-  port: dbconfig.development.port,
-  operatorsAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+const dbconnection = new Sequelize(
+  dbconfig.development.database, 
+  dbconfig.development.username, 
+  dbconfig.development.password, 
+  {
+    host: dbconfig.development.host,
+    dialect: dbconfig.development.dialect,
+    port: dbconfig.development.port,
+    operatorsAliases: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
 });
 
 dbconnection.DataTypes = Sequelize;
