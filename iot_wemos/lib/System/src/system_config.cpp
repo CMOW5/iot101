910a949301ea0b549cb19d4a9b834359b5b98cd1
CMOW5 <tec.cmos@gmail.com>
2018-08-30 20:40:32 -0500

    tosintring method added

--- iot_wemos/lib/System/src/system_config.cpp
+++ iot_wemos/lib/System/src/system_config.cpp
@@ -8,3 +8,102 @@ SystemConfig* SystemConfig::instance() {
   }
   return pInstance;
 }
+
+bool SystemConfig::writeConfigurationData(String jsonConfigurationData) {
+  // check the json configuration data
+  StaticJsonBuffer<512> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(jsonConfigurationData.c_str());
+
+  // Test if parsing succeeds.
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return false;
+  }
+
+  const char* wifiNetwork = root["wifiNetwork"];
+  const char* wifiPassword = root["wifiPassword"];
+  const char* mqttServer = root["mqttServer"];
+  const char* mqttPort = root["mqttPort"];
+  const char* mqttUser = root["mqttUser"];
+  const char* mqttKey = root["mqttKey"];
+  const char* mesa = root["mesa"];
+
+  this->writeWifiNetwork(wifiNetwork);
+  this->writeWifiPassword(wifiPassword);
+  this->writeMqttServerIp(mqttServer);
+  this->writeMqttPort(mqttPort);
+  this->writeMqttUsername(mqttUser);
+  this->writeMqttKey(mqttKey);
+  this->writeMesaNumber(mesa);
+  return true;
+}
+
+bool SystemConfig::writeWifiNetwork(String wifiNetwork) {
+  return eepromHandler.writeWifiNetwork(wifiNetwork);
+}
+
+bool SystemConfig::writeWifiPassword(String wifiPassword) {
+  return eepromHandler.writeWifiPassword(wifiPassword);
+}
+
+bool SystemConfig::writeMqttServerIp(String mqttIp) {
+  return eepromHandler.writeMqttServerIp(mqttIp);
+}
+
+bool SystemConfig::writeMqttPort(String mqttPort) {
+  return eepromHandler.writeMqttPort(mqttPort);
+}
+
+bool SystemConfig::writeMqttUsername(String mqttUsername) {
+  return eepromHandler.writeMqttUsername(mqttUsername);
+}
+
+bool SystemConfig::writeMqttKey(String mqttKey) {
+  return eepromHandler.writeMqttKey(mqttKey);
+}
+
+bool SystemConfig::writeMesaNumber(String mesaNumber) {
+  return eepromHandler.writeMesaNumber(mesaNumber);
+}
+
+String SystemConfig::wifiNetwork() {
+  return eepromHandler.readWifiNetwork();
+}
+
+String SystemConfig::wifiPassword() {
+  return eepromHandler.readWifiPassword();
+}
+
+String SystemConfig::mqttServerIp() {
+  return eepromHandler.readMqttServerIp();
+}
+
+String SystemConfig::mqttPort() {
+  return eepromHandler.readMqttPort();
+}
+
+String SystemConfig::mqttUsername() {
+  return eepromHandler.readMqttUsername();
+}
+
+String SystemConfig::mqttKey() {
+  return eepromHandler.readMqttKey();
+}
+
+String SystemConfig::mesaNumber() {
+  return eepromHandler.readMesaNumber();
+}
+
+String SystemConfig::toString(void) {
+  String output = "";
+  output += "wifi network = " + String(this->wifiNetwork());
+/*
+  output += "wifi pass = " + this->wifiPassword();
+  output += "mqtt server ip pass = " + this->mqttServerIp();
+  output += "mqtt port = " + this->mqttPort();
+  output += "mqtt username = " + this->mqttUsername();
+  output += "mqtt key = " + this->mqttKey();
+  output += "mesa number = " + this->mesaNumber();
+  */
+  return output;
+}
--- iot_wemos/lib/System/src/system_config.h
+++ iot_wemos/lib/System/src/system_config.h
@@ -9,22 +9,41 @@
 #ifndef _SYSTEM_CONFIG_H_
 #define _SYSTEM_CONFIG_H_
 
+#include "../../EEPROMHandler/src/EEPROM_Handler.h"
+#include "ArduinoJson.h"
+
 class SystemConfig {
   public:
     static SystemConfig* instance();
 
-    // config variables
-    // wifi
-    const char* WIFI_SSID = "TEC4825"; // network
-    const char* WIFI_PASS = "91121608540";
+    bool writeConfigurationData(String configurationData);
+    bool writeWifiNetwork(String wifiNetwork);
+    bool writeWifiPassword(String wifiPassword);
+    bool writeMqttServerIp(String mqttIp);
+    bool writeMqttPort(String mqttPort);
+    bool writeMqttUsername(String mqttUsername);
+    bool writeMqttKey(String mqttKey);
+    bool writeMesaNumber(String mesaNumber);
 
+    String wifiNetwork(void);
+    String wifiPassword(void);
+    String mqttServerIp(void);
+    String mqttPort(void);
+    String mqttUsername(void);
+    String mqttKey(void);
+    String mesaNumber(void);
+    String toString(void);
+
+    /*
     //mqtt
     const char* MQTT_SERVER = "192.168.0.13";
     int MQTT_SERVERPORT = 1883;
     const char* MQTT_USERNAME = "";
     const char* MQTT_KEY = "";
+    */
 
   private:
+    EEPROMHandler eepromHandler;
     static SystemConfig* pInstance;
 };
 
