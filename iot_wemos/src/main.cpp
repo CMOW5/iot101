0474b89096a5d8735c753458ebdb93852b44a8d9
CMOW5 <tec.cmos@gmail.com>
2018-09-02 20:46:18 -0500

    a ticker thread was added to check the serial buffer without causing a serial watch dog reset

--- iot_wemos/src/main.cpp
+++ iot_wemos/src/main.cpp
@@ -5,17 +5,22 @@
 #include "MQTT_Handler.h"
 #include "IOHandler.h"
 #include "EEPROM_Handler.h"
+#include "system_config.h"
+#include <Ticker.h>
+#include "SerialHandler.h"
 
 WifiHandler wifiHandler;
 MQTTHandler mqttHandler(wifiHandler);
 IOHandler ioHandler;
 Pins pins;
 
-EEPROMHandler eepromHandler;
+SystemConfig* systemConfig;
+Ticker serialPoll;
+
 // configuration loader
 String configurationData = "";         // a String to hold incoming data
 bool configurationReceived = false;  // whether the string is complete
-/*
+
 App app(
   &pins,
   &mqttHandler,
@@ -23,7 +28,7 @@ App app(
   mqttHandler.getEvents(),
   ioHandler.getEvents()
 );
-*/
+
 
 void initPins(void);
 void serialEvent(void);
@@ -31,63 +36,23 @@ void serialEvent(void);
 void setup() {
   // reserve 512 bytes for the configurationData: (max eeprom size)
   Serial.begin(9600);
+  // SerialHandler::disableLogs();
   delay(1000);
   configurationData.reserve(512);
-  // initPins();
-  // app.initialize();
-  Serial.println("start.........");
 
-  String eepromvalue1 = eepromHandler.readWifiNetwork();
-  String eepromvalue2 = eepromHandler.readWifiPassword();
-  String eepromvalue3 = eepromHandler.readMqttServerIp();
-  String eepromvalue4 = eepromHandler.readMqttPort();
-  String eepromvalue5 = eepromHandler.readMqttUsername();
-  String eepromvalue6 = eepromHandler.readMqttKey();
-  String eepromvalue7 = eepromHandler.readMesaNumber();
+  systemConfig = SystemConfig::instance();
+  SerialHandler::println("System config = ");
+  SerialHandler::println(systemConfig->toString());
 
-  Serial.println("wifinetwork = " + eepromvalue1);
-  Serial.println("wifipass = " + eepromvalue2);
-  Serial.println("mqttip = " + eepromvalue3);
-  Serial.println("mqttport = " + eepromvalue4);
-  Serial.println("mqttuser = " + eepromvalue5);
-  Serial.println("mqttkey = " + eepromvalue6);
-  Serial.println("mesa = " + eepromvalue7);
-  /*
-  eepromHandler.writeWifiNetwork("2 wifi-network 2");
-  eepromHandler.writeWifiPassword("2 wifi-password 2");
-  eepromHandler.writeMqttServerIp("2 mqtt-ip 2");
-  eepromHandler.writeMqttPort("2 mqtt-port 2");
-  eepromHandler.writeMqttUsername("2 mqtt-username 2");
-  eepromHandler.writeMqttKey("2 mqtt-key 2");
-  eepromHandler.writeMesaNumber("2 mesa-number 2");
-  eepromvalue1 = eepromHandler.readWifiNetwork();
-  eepromvalue2 = eepromHandler.readWifiPassword();
-  eepromvalue3 = eepromHandler.readMqttServerIp();
-  eepromvalue4 = eepromHandler.readMqttPort();
-  eepromvalue5 = eepromHandler.readMqttUsername();
-  eepromvalue6 = eepromHandler.readMqttKey();
-  eepromvalue7 = eepromHandler.readMesaNumber();
-  Serial.println("wifinetwork = " + eepromvalue1);
-  Serial.println("wifipass = " + eepromvalue2);
-  Serial.println("mqttip = " + eepromvalue3);
-  Serial.println("mqttport = " + eepromvalue4);
-  Serial.println("mqttuser = " + eepromvalue5);
-  Serial.println("mqttkey = " + eepromvalue6);
-  Serial.println("mesa = " + eepromvalue7);
-  */
+  // check the serial buffer every 1 ms
+  serialPoll.attach_ms(20, serialEvent);
+
+  initPins();
+  app.initialize();
 }
 
 void loop() {
-  // app.tasks();
-/*
-  serialEvent();
-  if (configurationReceived) {
-    Serial.println(configurationData);
-    // clear the configuration:
-    configurationData = "";
-    configurationReceived = false;
-  }
-  */
+  app.tasks();
 }
 
 void initPins() {
@@ -104,16 +69,23 @@ void initPins() {
   delay response. Multiple bytes of data may be available.
 */
 void serialEvent() {
-  return;
-  while (Serial.available()) {
+  if (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
+
     // add it to the configurationData:
     configurationData += inChar;
+
     // if the incoming character is a newline, set a flag so the main loop can
     // do something about it:
-    if (inChar == '\n') {
-      configurationReceived = true;
+    if (inChar == '\0') {
+      // configurationReceived = true;
+      if (systemConfig->writeConfigurationData(configurationData)) {
+        Serial.print(1);
+      } else {
+        Serial.print(0);
+      }
+      configurationData = "";
     }
   }
 }
